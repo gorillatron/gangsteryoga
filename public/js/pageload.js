ca72399d96530b290621eda9d64be69e2890dda0
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2016-05-19 15:09:03 +0200

    werk werk

--- public/js/pageload.js
+++ public/js/pageload.js
@@ -1,8 +1,41 @@
 
-$(document).ready(function(){
 
-  setTimeout(function() {
-    $("html").css("opacity", 1)
-  }, 66)
-
-})
+!(function() {
+  
+  var Observable = Rx.Observable
+  
+  var text = "For de som tørr å møte seg selv."
+  var words = text.split(" ")
+  
+  var input = document.querySelectorAll("#hero h1 textarea")[0]
+  
+  var words$ = Observable.from(words)
+  
+  var typestream$ = words$
+    .concatMap(function(x) {
+      return Observable.of(x)
+        .delay(
+          (Math.random() * 170) + (x.length * 1.5)
+        )
+    })
+    .concatMap(function(word) {
+      var letters$ = Observable.from(word.split("").concat(" "))
+      var typestream$ = letters$
+        .concatMap(function(letter) {
+          return Observable.of(letter)
+            .delay(
+              (Math.random() * 110)
+            )
+        })
+      return typestream$
+    })
+  
+  input.focus()
+  
+  typestream$
+    .delay(937)
+    .subscribe(function(char) {
+      input.value = input.value + char
+    })
+  
+})();

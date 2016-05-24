382d20ff50a93e0a3e99379da2d055e0274a3ac0
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2016-05-24 20:14:51 +0200

    gallery

--- clientsrc/entries/gallery.js
+++ clientsrc/entries/gallery.js
@@ -1,3 +1,13 @@
 
 import {Observable, Scheduler} from "rxjs"
 
+const gallery = document.querySelectorAll(".gallery")[0]
+
+const mousein$ = Observable.fromEvent(gallery, "mousein")
+const mouseout$ = Observable.fromEvent(gallery, "mouseout")
+
+const mousemoves$ = Observable.fromEvent(gallery, 'mousemove')
+
+const mousepos$ = mousemoves$
+  .throttleTime(33)
+  .map(ev => ({x: ev.clientX, y: ev.clientY}))
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
   entry: {
     hero: path.join(entrypath, 'hero.js'),
     menu: path.join(entrypath, 'menu.js'),
+    gallery: path.join(entrypath, 'gallery.js')
   },
   
   output: {

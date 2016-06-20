bce83ac68a0accea4f9d282c00a1743380b61fde
gorillatron <jornandretangen@gmail.com>
2016-06-20 14:27:38 +0200

    heya

--- clientsrc/entries/scroll.js
+++ clientsrc/entries/scroll.js
@@ -2,15 +2,33 @@
 import raf from "raf"
 import {Observable, Scheduler} from "rxjs"
 import ensurepos from "../lib/ensurepos"
+import currentwindowsize from "../lib/currentwindowsize"
+import {translate} from "../lib/css"
 
+const seperatorHeight = 240
+const seperatorPadding = 40
 
-const container = document.getElementById("root-container")
+const parallaxImagesContainer = document.querySelector("#parallax-images")
+const parallaxImages = document.querySelectorAll("#parallax-images .img")
+const fullBlocks = document.querySelectorAll("#content .block.full")
 const scrolls$ = Observable.fromEvent(window, "mousewheel")
+const windowsizes$ = Observable.fromEvent(window, 'resize')
+  .throttleTime(33)
+  .map(ev => currentwindowsize())
+  .startWith(currentwindowsize())
 
+const parallaxImageSize$ = windowsizes$.map(size => size)
 
-const flipStarts$ = scrolls$
-  .filter((ev) => ev.deltaY > 20 || ev.deltaY < -20)
+parallaxImageSize$.subscribe(size => {
+  parallaxImages.forEach(img => {
+    img.style.height = size.y + ((seperatorHeight - seperatorPadding) / 2) + "px"
+  })
+  fullBlocks.forEach(block => {
+    block.style.height = size.y + "px"
+  })
+})
 
-flips$.subscribe(ev => {
-  console.log(ev.deltaY) 
+scrolls$.subscribe(event => {
+  const y = document.body.scrollTop * 0.8
+  translate(parallaxImagesContainer, 0, -y)
 })

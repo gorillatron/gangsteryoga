9225d618ff94ad11f604c294e245cef038ef5d44
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2016-07-25 16:01:20 +0200

    werk

--- clientsrc/js/entries/menu.js
+++ clientsrc/js/entries/menu.js
@@ -1,8 +1,20 @@
 
 const sidebar = $("#main-sidebar")
-const toggleButton = $("#menu-toggle")
+const toggleButton = $("#cover .hamburger")
+const nav = $("nav")
+const cover = $("#cover")
+
+let open = false
 
 toggleButton.on("click", event => {
-  sidebar.toggleClass("open")
+  open = !open
+  console.log(nav.width())
+  if(open) {
+    cover.css('transform', 'translatex(-' + nav.outerWidth() + 'px)')
+  }
+  else {
+    cover.css('transform', 'translatex(0px)')
+  }
+  cover.toggleClass("show-menu")
   toggleButton.toggleClass("cross")
 })

40346ccfbdc8f9d03c66b6e1fd73b5e53c140fa2
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2016-07-25 16:06:51 +0200

    tweaks

--- clientsrc/js/entries/menu.js
+++ clientsrc/js/entries/menu.js
@@ -1,20 +1,29 @@
 
 const sidebar = $("#main-sidebar")
 const toggleButton = $("#cover .hamburger")
-const nav = $("nav")
+const nav = $("#main-nav")
 const cover = $("#cover")
 
 let open = false
 
 toggleButton.on("click", event => {
   open = !open
-  console.log(nav.width())
+  
+  let transform = null
+
   if(open) {
-    cover.css('transform', 'translatex(-' + nav.outerWidth() + 'px)')
+    transform = 'translatex(-' + nav.outerWidth() + 'px)'
   }
   else {
-    cover.css('transform', 'translatex(0px)')
+    transform = 'translatex(0px)'
   }
-  cover.toggleClass("show-menu")
+  
+  cover.css({
+    '-ms-transform': transform,
+    '-moz-transform': transform,
+    '-webkit-transform': transform,
+    'transform': transform
+  })
+
   toggleButton.toggleClass("cross")
 })

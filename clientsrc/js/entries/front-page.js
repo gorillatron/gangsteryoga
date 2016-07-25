3f5814d2fdd573e7233e3b72f13c4d82b632a166
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2016-07-25 18:09:29 +0200

    werk

--- clientsrc/js/entries/front-page.js
+++ clientsrc/js/entries/front-page.js
@@ -2,29 +2,56 @@
 import page from 'page'
 import raf from 'raf'
 
+
 let openPage = null
+
+
+const animatePageIn = ($page, cb) => {
+  $page.show()
+  raf(_ => {
+    $page.animate({
+      opacity: 1
+    }, 137, cb)
+  })
+}
+
+
+const animatePageOut = ($page, cb = f => f) => {
+  raf(_ => {
+    $page.animate({
+      opacity: 0
+    }, 137, _ => {
+      $page.hide()
+      cb()
+    })
+  })
+}
+
 
 page('/', (ctx, next) => {
   if(openPage) {
-    openPage.removeClass('pre-open open').addClass('closed')
+    animatePageOut(openPage)
   }
 })
 
+
 page('*', (ctx, next) => {
   if(openPage) {
-    openPage.removeClass('pre-open open').addClass('closed')
+    animatePageOut(openPage, next)
   }
-  next()
+  else {
+    next()
+  }
 })
+
 
 page('/:page', (ctx, next) => {
   const {page} = ctx.params
   const $page = $(`.content-container.${page}`)
 
   openPage = $page
-
-  raf(_ => $page.addClass('pre-open'))
-  setTimeout(_ => $page.addClass('open'), 33)
+  console.log("LOL")
+  raf(_ => animatePageIn($page))
   
 })
 

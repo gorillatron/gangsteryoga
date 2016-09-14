f6dc93c89dda2f39ce0a25cdcb0868e055fb9823
gorillatron <jornandretangen@gmail.com>
2016-09-14 10:48:08 +0200

    werk

--- clientsrc/js/entries/front-page/index.js
+++ clientsrc/js/entries/front-page/index.js
@@ -2,6 +2,9 @@
 import routing from './routing'
 import parallaxing from './parallaxing'
 import scrolling from './scrolling'
+import Player from '@vimeo/player'
+import raf from 'raf'
+
 
 routing()
 
@@ -16,5 +19,32 @@ $(document).ready(() => {
   
   scrolling()
   parallaxing()
+
+  const playButton = $('#featured .play-button')
+  const movieMode = $('#main-header .movie-mode')
+  const video = $('.video')
+  const videoFrame = $('.video iframe')
+  const player = new Player(videoFrame)
+
+  playButton.click(() => {
+    player.play()
+  })
+
+  const onPlay = () => {
+    raf(() => playButton.addClass('hide'))
+    raf(() => movieMode.addClass('active'))
+    raf(() => video.addClass('playing'))
+    raf(() => videoFrame.removeClass('hidden'))
+    $('body').one('click', () => player.pause())
+  }
+
+  const onStop = () => {
+    raf(() => video.removeClass('playing'))
+    raf(() => movieMode.removeClass('active'))
+  }
+
+  player.on('play', onPlay)
+  player.on('pause', onStop)
+  player.on('stop', onStop)
 
 })

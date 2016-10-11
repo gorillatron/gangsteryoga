44414a97ddbfe18380aaa68545b1e228f032cac0
gorillatron <jornandretangen@gmail.com>
2016-10-11 11:19:27 +0530

    menu

--- clientsrc/js/entries/menu/index.js
+++ clientsrc/js/entries/menu/index.js
@@ -1,47 +1,47 @@
 
 
-const menu = $("#main-menu")
-const toggleButton = $(".hamburger")
-const nav = $("#main-nav")
-const cover = $("#cover")
-
-let menuCloseTime = 5 * 1000
-let state = {open: false, closeTimer: null}
+$(document).ready(() => {
 
 
-toggleButton.on("click", event => {
-  setMenu(!state.open)
+  const menu = $("#main-menu")
+  const toggleButton = $(".hamburger")
+
+  let menuCloseTime = 5 * 1000
+  let state = {open: false, closeTimer: null}
+
+
+  toggleButton.on("click", event => {
+    setMenu(!state.open)
+  })
+
+  menu.on('click', (event) => {
+    if(menu.hasClass('closed')) {
+      event.preventDefault()
+      return false
+    }
+  })
+
+  const setMenu = open => {
+    
+    state.open = open
+
+    $(document).off('click.menuclose')
+
+    if(state.closeTimer) {
+      clearTimeout(state.closeTimer)
+      state.closeTimer = null
+    }
+
+    if(state.open) {
+      menu.addClass("open")
+      toggleButton.addClass('cross black')
+      state.closeTimer = setTimeout(_ => setMenu(false), menuCloseTime)
+    }
+    else {
+      menu.removeClass("open")
+      toggleButton.removeClass('cross black')
+    }
+  }
+
+
 })
-
-menu.on('click', (event) => {
-  if(menu.hasClass('closed')) {
-    event.preventDefault()
-    return false
-  }
-})
-
-const setMenu = open => {
-  let transform = null
-
-  state.open = open
-
-  $(document).off('click.menuclose')
-
-  const menuHeight = menu.innerHeight()
-
-  if(state.closeTimer) {
-    clearTimeout(state.closeTimer)
-    state.closeTimer = null
-  }
-
-  if(state.open) {
-    menu.css({ top: `-${(menuHeight / 2) / 2}px` })
-    toggleButton.addClass('close')
-    menu.removeClass('closed')
-    state.closeTimer = setTimeout(_ => setMenu(false), menuCloseTime)
-  }
-  else {
-    menu.addClass('closed')
-    toggleButton.removeClass('close')
-  }
-}
